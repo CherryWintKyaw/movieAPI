388cb4153ad88c0c32cf31cb0ba8ce0ea6316472
CherryWintKyaw <wintkyawcherry@gmail.com>
2026-05-08 11:18:16 +0630

    media

--- movieAPI/settings.py
+++ movieAPI/settings.py
@@ -151,3 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media files configuration (အခု ဒါလေး ထည့်ပေးပါ)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
